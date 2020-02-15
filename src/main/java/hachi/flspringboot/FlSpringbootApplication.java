c46dc9a904c4d9188519867e1d42c6d1cc28b9f8
hachi <eojin312@naver.com>
2020-02-15 15:23:51 +0900

    첫 장 hello 가 json 형식으로 리턴되는 걸 확인

---
+++ src/main/java/hachi/flspringboot/FlSpringbootApplication.java
@@ -0,0 +1,13 @@
+package hachi.flspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlSpringbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hachi/flspringboot/FlSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package hachi.flspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
