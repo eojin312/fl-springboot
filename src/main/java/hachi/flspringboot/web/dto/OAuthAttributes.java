e6e463cc2c4b70c095e8fffdc17dd7ed70104476
hachi <eojin312@naver.com>
2020-03-03 15:43:41 +0900

    Oauth2 Naver 추가

--- src/main/java/hachi/flspringboot/web/dto/OAuthAttributes.java
+++ src/main/java/hachi/flspringboot/web/dto/OAuthAttributes.java
@@ -34,6 +34,9 @@ public class OAuthAttributes {
      * @return
      */
     public static OAuthAttributes of(String registrationId, String userNameAttributeName, Map<String, Object> attributes) {
+        if ("naver".equals(registrationId)) {
+            return ofNaver("id", attributes);
+        }
         return ofGoogle(userNameAttributeName, attributes);
     }
 
@@ -44,6 +47,17 @@ public class OAuthAttributes {
                 .picture((String) attributes.get("picture"))
                 .attributes(attributes)
                 .nameAttributeKey(userNameAttributeName)
+                .build();
+    }
+
+    public static OAuthAttributes ofNaver(String userNameAttribute, Map<String, Object> attributes) {
+        Map<String, Object> response = (Map<String, Object>) attributes.get("response");
+        return OAuthAttributes.builder()
+                .name((String) response.get("name"))
+                .email((String) response.get("email"))
+                .picture((String) response.get("picture"))
+                .attributes(response)
+                .nameAttributeKey(userNameAttribute)
                 .build();
     }
 
